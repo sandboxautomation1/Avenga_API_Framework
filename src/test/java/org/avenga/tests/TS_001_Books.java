ad7965564759d1ce0afce1eb142475a8b9e1c4a2
QINSHIFT\Pavel.Popov <pavel.popov@avenga.com>
2026-03-25 15:39:49 +0200

    add test descriptions

--- src/test/java/org/avenga/tests/TS_001_Books.java
+++ src/test/java/org/avenga/tests/TS_001_Books.java
@@ -15,57 +15,57 @@ public class TS_001_Books extends BaseTest {
     Books books = new Books();
 
 
-    @Test(testName = "[TC_001_Books] Retrieve a list of all books", description = "GET", priority = 0)
+    @Test(testName = "[TC_001_Books] Retrieve a list of all books", description = "Ensure the API returns accurate HTTP status codes for valid GET request.", groups = {"GET"}, priority = 0)
     public void TC_001_GetAllBooks() throws Exception {
         books.GET_AllBooks(200);
     }
 
 
-    @Test(testName = "[TC_002_Books] Retrieve details of a specific book by its ID", description = "GET", priority = 1)
+    @Test(testName = "[TC_002_Books] Retrieve details of a specific book by its ID", description = "Validate that READ operation work as expected with valid ID.", groups = {"GET"}, priority = 1)
     public void TC_002_GetBookByID() throws Exception {
         books.GET_BookById(200, 1);
     }
 
 
-    @Test(testName = "[TC_003_Books] Add a new book to the system", description = "POST", priority = 2)
+    @Test(testName = "[TC_003_Books] Add a new book to the system", description = "Validate that CREATE operation work as expected with valid payload.", groups = {"POST"}, priority = 2)
     public void TC_003_AddBook() throws Exception {
         books.POST_CreateBook(200, Book.createRandomBookPayload());
     }
 
 
-    @Test(testName = "[TC_004_Books] Update an existing book by its ID", description = "PUT", priority = 3)
+    @Test(testName = "[TC_004_Books] Update an existing book by its ID", description = "Validate that UPDATE operation work as expected with valid payload.", groups = {"PUT"}, priority = 3)
     public void TC_004_UpdateBook() throws Exception {
         books.PUT_UpdateBookById(200, Book.createRandomBookPayload(), 1);
     }
 
 
-    @Test(testName = "[TC_005_Books] Delete a book by its ID", description = "DELETE", priority = 4)
+    @Test(testName = "[TC_005_Books] Delete a book by its ID", description = "Validate that DELETE operation work as expected with valid ID.", groups = {"DELETE"}, priority = 4)
     public void TC_005_DeleteBook() throws Exception {
         books.DELETE_DeleteBookById(200, 1);
     }
 
 
-    @Test(testName = "[TC_006_Books] Retrieve a book with invalid ID", description = "GET", priority = 5)
+    @Test(testName = "[TC_006_Books] Retrieve a book with invalid ID", description = "Validate that READ operation work as expected with invalid ID.", groups = {"GET"}, priority = 5)
     public void TC_006_GetBookWithInvalidId() throws Exception {
         books.GET_BookById(404, 201);
     }
 
 
-    @Test(testName = "[TC_007_Books] Add a new book to the system with missing publishing date", description = "POST", priority = 6)
+    @Test(testName = "[TC_007_Books] Add a new book to the system with missing publishing date", description = "Validate that CREATE operation work as expected with invalid payload.", groups = {"POST"}, priority = 6)
     public void TC_007_AddBookWithInvalidPayload() throws Exception {
         JsonObject body = Book.createBookPayload(1, "Title,", "Description", 25, "", null);
         books.POST_CreateBook(400, body);
     }
 
 
-    @Test(testName = "[TC_008_Books] Update book with missing publishing date", description = "PUT", priority = 7)
+    @Test(testName = "[TC_008_Books] Update book with missing publishing date", description = "Validate that UPDATE operation work as expected with invalid payload.", groups = {"PUT"}, priority = 7)
     public void TC_008_UpdateBookWithInvalidPayload() throws Exception {
         JsonObject body = Book.createBookPayload(1, "Title,", "Description", 25, "", null);
         books.PUT_UpdateBookById(400, body, 1);
     }
 
 
-    @Test(testName = "[TC_009_Books] Delete non existing record", description = "DELETE", priority = 8)
+    @Test(testName = "[TC_009_Books] Delete non existing record", description = "Validate that DELETE operation work as expected with invalid payload.", groups = {"DELETE"}, priority = 8)
     public void TC_009_DeleteNonExistingBook() throws Exception {
         books.DELETE_DeleteBookById(400, null);
     }
--- src/test/java/org/avenga/tests/TS_002_Authors.java
+++ src/test/java/org/avenga/tests/TS_002_Authors.java
@@ -15,57 +15,57 @@ public class TS_002_Authors extends BaseTest {
    Authors authors =  new Authors();
 
 
-    @Test(testName = "[TC_001_Authors] Retrieve a list of all authors", description = "Retrieve a list of all authors by sending correct request", groups = {"GET"}, priority = 0)
+    @Test(testName = "[TC_001_Authors] Retrieve a list of all authors", description = "Ensure the API returns accurate HTTP status codes for valid GET request.", groups = {"GET"}, priority = 0)
     public void TC_001_GetAllAuthors() throws Exception {
         authors.GET_AllAuthors(200);
     }
 
 
-    @Test(testName = "[TC_002_Authors] Retrieve details of a specific author by its ID", description = "GET", priority = 1)
+    @Test(testName = "[TC_002_Authors] Retrieve details of a specific author by its ID", description = "Validate that READ operation work as expected with valid ID.", groups = {"GET"}, priority = 1)
     public void TC_002_GetAuthorByID() throws Exception {
         authors.GET_AuthorById(200, 1);
     }
 
 
-    @Test(testName = "[TC_003_Authors] Add a new author to the system", description = "POST", priority = 2)
+    @Test(testName = "[TC_003_Authors] Add a new author to the system", description = "Validate that CREATE operation work as expected with valid payload.", groups = {"POST"}, priority = 2)
     public void TC_003_AddAuthor() throws Exception {
         authors.POST_AddAuthor(200, Author.createRandomAuthorPayload());
     }
 
 
-    @Test(testName = "[TC_004_Authors] Update an existing author’s details by its ID", description = "PUT", priority = 3)
+    @Test(testName = "[TC_004_Authors] Update an existing author’s details by its ID", description = "Validate that UPDATE operation work as expected with valid payload.", groups = {"PUT"}, priority = 3)
     public void TC_004_UpdateAuthor() throws Exception {
         authors.PUT_UpdateAuthorById(200, Author.createRandomAuthorPayload(), 1);
     }
 
 
-    @Test(testName = "[TC_005_Authors] Delete a author by its ID", description = "DELETE", priority = 4)
+    @Test(testName = "[TC_005_Authors] Delete a author by its ID", description = "Validate that DELETE operation work as expected with valid ID.", groups = {"DELETE"}, priority = 4)
     public void TC_005_DeleteAuthor() throws Exception {
         authors.DELETE_DeleteAuthorById(200, 1);
     }
 
 
-    @Test(testName = "[TC_006_Authors] Retrieve a author with invalid ID", description = "GET", priority = 5)
+    @Test(testName = "[TC_006_Authors] Retrieve a author with invalid ID", description = "Validate that READ operation work as expected with invalid ID.", groups = {"GET"}, priority = 5)
     public void TC_006_GetAuthorWithInvalidId() throws Exception {
         authors.GET_AuthorById(400, null);
     }
 
 
-    @Test(testName = "[TC_007_Authors] Add a new author with invalid book ID", description = "POST", priority = 6)
+    @Test(testName = "[TC_007_Authors] Add a new author with invalid book ID", description = "Validate that CREATE operation work as expected with invalid payload.", groups = {"POST"}, priority = 6)
     public void TC_007_AddAuthorWithInvalidPayload() throws Exception {
         JsonObject body = Author.createAuthorPayload(1, null, "", "");
         authors.POST_AddAuthor(400, body);
     }
 
 
-    @Test(testName = "[TC_008_Authors] Update an existing author’s details with invalid book ID", description = "PUT", priority = 7)
+    @Test(testName = "[TC_008_Authors] Update an existing author’s details with invalid book ID", description = "Validate that UPDATE operation work as expected with invalid payload.", groups = {"PUT"}, priority = 7)
     public void TC_008_UpdateAuthorWithInvalidPayload() throws Exception {
         JsonObject body = Author.createAuthorPayload(1, 1, "", "");
         authors.PUT_UpdateAuthorById(400, body, null);
     }
 
 
-    @Test(testName = "[TC_009_Authors] Delete non existing author", description = "DELETE",  priority = 8)
+    @Test(testName = "[TC_009_Authors] Delete non existing author", description = "Validate that DELETE operation work as expected with invalid ID.", groups = {"DELETE"}, priority = 8)
     public void TC_009_DeleteNonExistingAuthor() throws Exception {
         authors.DELETE_DeleteAuthorById(400, null);
     }
