04f55848802ba7a69d23267d46630e079230e15d
QINSHIFT\Pavel.Popov <pavel.popov@avenga.com>
2026-03-29 18:47:53 +0300

    fix logo path

--- src/main/java/org/avenga/data/FrameworkConstants.java
+++ src/main/java/org/avenga/data/FrameworkConstants.java
@@ -15,5 +15,5 @@ public class FrameworkConstants {
 
     public static final String REPORT_ARCHIVE_FILE = "test-output-archive/Automation_Report.zip";
     public static final String REPORT_HTML_FILE    = "./test-output/index.html";
-    public static final String REPORT_LOGO_FILE    = "./src/main/java/resources/logo.png";
+    public static final String REPORT_LOGO_FILE    = "../src/main/java/resources/logo.png";
 }
